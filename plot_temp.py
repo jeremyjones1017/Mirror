7a98dfe67613b5365ccd106256321e2221180720
Jeremy Jones <rejemy1017@gmail.com>
2020-10-29 22:37:16 -0400

    Tested plot_temp.py and got it going

--- plot_temp.py
+++ plot_temp.py
@@ -5,12 +5,18 @@ from datetime import timedelta
 
 def main():
 	now = datetime.datetime.now()
-	today_df = pd.read_csv('records/{}_{}_{}_temp_hum.txt'.format(now.year,now.month,now.day))
+	today_df = pd.read_csv('records/{}_{}_{}_temp_hum.txt'.format(now.year,now.month,now.day),delimiter='\t')
 	yesterday = now - datetime.timedelta(days=1)
-	yest_df = pd.read_csv('records/{}_{}_{}_temp_hum.txt'.format(yesterday.year,yesterday.month,yesterday.day))
+	yest_df = pd.read_csv('records/{}_{}_{}_temp_hum.txt'.format(yesterday.year,yesterday.month,yesterday.day),delimiter='\t')
 	
-	plt.plot(today_df.Time,today_df.Temp_F,k-)
-	plt.plot(yest_df.Time,yest_df.Temp_F,r-)
+	today_times = list(today_df.Time)
+	today_times = [datetime.datetime.strptime(time,'%Y-%m-%d %H:%M:%S') for time in today_times]
+	
+	yest_times = list(yest_df.Time)
+	yest_times = [datetime.datetime.strptime(time,'%Y-%m-%d %H:%M:%S') for time in yest_times]
+	
+	plt.plot(today_times,today_df.Temp_F,'k-')
+	plt.plot(yest_times,yest_df.Temp_F,'r-')
 	
 	plt.show()
 	
