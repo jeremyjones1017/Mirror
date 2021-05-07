df1fa13cd9e4fe00fbcc1b38b9fef721f2eea2d0
Jeremy Jones <rejemy1017@gmail.com>
2021-05-06 22:15:05 -0400

    Added start_mirror.py functionality to mirror.py

--- mirror.py
+++ mirror.py
@@ -12,6 +12,7 @@ from noaa_sdk import NOAA
 import pytz
 import wget
 import os
+import csv
 
 #Colors
 black=(0,0,0)
@@ -24,6 +25,40 @@ yellow=(255,255,0)
 purple=(128,0,128)
 
 def main():
+	mirror_active = check_mirror_state()
+	print(mirror_active)
+	
+	if mirror_active == False:
+		start_mirror()
+		
+def check_mirror_state():
+	mirror_command = 'mirror.py'
+	ps_command = 'ps aux | grep "{}" > tmp'.format(mirror_command)
+	os.system(ps_command)
+	
+	ps_output = []
+	with open('tmp','r') as input_file:
+		input_file_reader = csv.reader(input_file,delimiter = '\t')
+		for line in input_file_reader:
+			ps_output.append(line)
+	
+	os.remove('tmp')
+	
+	mirror_active = False
+	n_open = 0
+	for line in ps_output:
+		if mirror_command in line[0] and 'grep' not in line[0]:
+			n_open+=1
+			#print('1',line)
+		#else:
+		#	print('0',line)
+	if n_open > 2:
+		mirror_active = True
+	
+	return mirror_active	
+	
+	
+def start_mirror():
 	root = tk.Tk()
 	pygame.init()
 	FPS = 1
